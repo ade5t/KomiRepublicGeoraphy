d09b922ef6779fba3a0e3d2a683448eb1c00e345
ade5t <33578909+ade5t@users.noreply.github.com>
2017-11-11 22:04:28 +0300

    Add files via upload

---
+++ js/MyScripts.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+		var i=0;
+		function SlideSprav(col)
+		{"use strict";
+		 	for (var k=0; k<=col; k++) {
+  			if (k===i) {document.getElementById(String(k)).style.display="block";}
+				else {document.getElementById(String(k)).style.display="none";}
+			}
+		}	
+		
+		function PrevSprav(col,link)
+		{"use strict";
+			if (i>0)
+			{--i;
+			 SlideSprav(col);
+			}
+		 else {location.href=link;}
+		}
+		
+		function NextSprav(col,link)
+		{"use strict";
+		 if (i<col)
+			{++i;
+			 SlideSprav(col);
+			}
+		 else {location.href=link;}
+		}
