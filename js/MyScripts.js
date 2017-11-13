6514bcd22eb4b6bc852d6f7cbc071deaae33a6e7
ade5t <33578909+ade5t@users.noreply.github.com>
2017-11-13 23:29:28 +0300

    Add files via upload

--- js/MyScripts.js
+++ js/MyScripts.js
@@ -25,3 +25,31 @@
 			}
 		 else {location.href=link;}
 		}
+
+		var slideIndex = 1;
+
+		function plusDivs(n,num) {
+		  	showDivs(slideIndex += n,num);
+		}
+
+		function currentDiv(n,num) {
+		  	showDivs(slideIndex = n,num);
+		}
+
+		function showDivs(n,num) {
+		var i;
+		var x = document.getElementsByClassName("mySlides"+' '+String(num));
+		var dots = document.getElementsByClassName("demo");
+			if (n > x.length) {slideIndex = 1}    
+			if (n < 1) {slideIndex = x.length}
+			for (i = 0; i < x.length; i++) {
+		 		x[i].style.display = "none";  
+			}
+			for (i = 0; i < dots.length; i++) {
+		 		dots[i].className = dots[i].className.replace("w3-dark-grey", "");
+		  		dots[slideIndex-1].className += "w3-transparent";
+			}
+			x[slideIndex-1].style.display = "block";  
+			/*dots[slideIndex-1].className = dots[slideIndex-1].className.replace("w3-transparent", "");
+			dots[slideIndex-1].className += "w3-dark-grey";*/
+		}
